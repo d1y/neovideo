941bbb609e1a750e0cd97a78b7eb2285acbffb70
d1y <chenhonzhou@gmail.com>
2023-09-22 08:39:38 +0800

    frontend: add vscode debugger config

--- frontend/admin/src/App.vue
+++ frontend/admin/src/App.vue
@@ -1,6 +1,17 @@
 <script setup lang="ts">
+import { onMounted, ref } from 'vue';
+
+const msg = ref("hello world")
+
+onMounted(()=> {
+  msg.value = "hello world 1"
+})
+
+function d() {
+  debugger
+}
 </script>
 
 <template>
-  <div> hello world</div>
+  <div @click="d"> {{ msg }} </div>
 </template>
