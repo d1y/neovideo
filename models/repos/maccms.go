86dad0a4b13d60b161e5426e7cfafebe2f6893f7
d1y <chenhonzhou@gmail.com>
2023-09-24 23:00:44 +0800

    models/repo: export IMacCMS iface

--- models/repos/maccms.go
+++ models/repos/maccms.go
@@ -7,13 +7,19 @@ import (
 	"gorm.io/gorm"
 )
 
+type IMacCMS struct {
+	Api         string                      `json:"api,omitempty" gorm:"api"`
+	Name        string                      `json:"name,omitempty" gorm:"name"`
+	R18         bool                        `json:"r_18,omitempty" gorm:"r_18"`
+	LastCheck   time.Time                   `json:"last_check,omitempty" gorm:"last_check"`
+	Available   bool                        `json:"available,omitempty" gorm:"available"`
+	RespType    int                         `json:"resp_type,omitempty" gorm:"resp_type"`
+	Category    datatypes.JSONSlice[string] `json:"category,omitempty" gorm:"category"`
+	JiexiURL    string                      `json:"jiexi_url,omitempty" gorm:"jiexi_url"`
+	JiexiEnable bool                        `json:"jiexi_enable,omitempty" gorm:"jiexi_enable"`
+}
+
 type MacCMSRepo struct {
 	gorm.Model
-	Api       string                      `json:"api,omitempty" gorm:"api"`
-	Name      string                      `json:"name,omitempty" gorm:"name"`
-	R18       bool                        `json:"r_18,omitempty" gorm:"r_18"`
-	LastCheck time.Time                   `json:"last_check,omitempty" gorm:"last_check"`
-	Available bool                        `json:"available,omitempty" gorm:"available"`
-	RespType  int                         `json:"resp_type,omitempty" gorm:"resp_type"`
-	Category  datatypes.JSONSlice[string] `json:"category,omitempty" gorm:"category"`
+	IMacCMS
 }
