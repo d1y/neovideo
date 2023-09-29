4955f242697a2afb24be102d05f29aa9f558e7fe
d1y <chenhonzhou@gmail.com>
2023-09-29 23:01:34 +0800

    web: add /siteinfo api(show current register routes-meta)

--- app.go
+++ app.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 
 	"d1y.io/neovideo/config"
+	"d1y.io/neovideo/controllers/base"
 	baseControllers "d1y.io/neovideo/controllers/base"
 	jiexiControllers "d1y.io/neovideo/controllers/jiexi"
 	maccmsControllers "d1y.io/neovideo/controllers/maccms"
@@ -45,6 +46,10 @@ func (na *NeovideoApp) GetDB() *sql.DB {
 }
 
 func (na *NeovideoApp) Register() {
+	na.App.Get("/siteinfo", func(i iris.Context) {
+		routeMeta := na.App.GetRoutesReadOnly()
+		base.RenderSiteInfo(i, routeMeta)
+	})
 	na.App.PartyFunc("/api/v1", func(u iris.Party) {
 		u.PartyFunc("/base", baseControllers.Register)
 		u.PartyFunc("/maccms", maccmsControllers.Register)
--- controllers/base/index.go
+++ controllers/base/index.go
@@ -5,7 +5,17 @@ import (
 	"time"
 
 	"github.com/kataras/iris/v12"
+	"github.com/kataras/iris/v12/context"
 )
+
+type routeMeta struct {
+	Name string `json:"name,omitempty"`
+	Path string `json:"path,omitempty"`
+}
+
+type siteInfo struct {
+	Route []routeMeta `json:"route_meta,omitempty"`
+}
 
 type BaseController struct {
 	iris.Context
@@ -19,4 +29,17 @@ func (c *BaseController) ping(ctx iris.Context) {
 func Register(u iris.Party) {
 	var bc BaseController
 	u.Get("/ping", bc.ping).Name = "调试"
+}
+
+func RenderSiteInfo(p iris.Context, meta []context.RouteReadOnly) {
+	var result = make([]routeMeta, len(meta))
+	for idx, item := range meta {
+		result[idx] = routeMeta{
+			Name: item.Name(),
+			Path: item.Path(),
+		}
+	}
+	p.JSON(siteInfo{
+		Route: result,
+	})
 }
