2a33612531daa93e4a1403287427ee001409a6be
d1y <chenhonzhou@gmail.com>
2023-10-04 14:10:47 +0800

    maccms: miss parse dd playurl is real-url
    或许播放链接真的是个 url 呢!

--- spider/implement/maccms/utils.go
+++ spider/implement/maccms/utils.go
@@ -1,9 +1,27 @@
 package maccms
 
-import "strings"
+import (
+	"regexp"
+	"strings"
+)
+
+const (
+	unknownPlayName = "未命名"
+)
+
+var mustURLReg = regexp.MustCompile(`^https?://[-A-Za-z0-9+&@#/%?=~_|!:,.;]+[-A-Za-z0-9+&@#/%=~_|]`)
 
 func parseDDRawURL(raw string) []IMacCMSVideoDDTagWithURL {
 	var result = make([]IMacCMSVideoDDTagWithURL, 0)
+	if mustURLReg.MatchString(raw) {
+		// 某些播放链接就是这么特殊, 就纯纯是个m3u8/mp4播放链接
+		// ^_^
+		result = append(result, IMacCMSVideoDDTagWithURL{
+			URL:  raw,
+			Name: unknownPlayName,
+		})
+		return result
+	}
 	for _, rawItem := range strings.Split(raw, "#") {
 		item := strings.Split(rawItem, "$")
 		if len(item) <= 1 {
