23e62f09ee4b34d6216ce89ea9a23feac5d4bb19
d1y <chenhonzhou@gmail.com>
2023-10-06 19:16:47 +0800

    vod: impl getDetail with ID

--- controllers/vod/index.go
+++ controllers/vod/index.go
@@ -7,6 +7,7 @@ import (
 	"d1y.io/neovideo/controllers/handler"
 	"d1y.io/neovideo/models/repos"
 	"d1y.io/neovideo/models/web"
+	"github.com/acmestack/gorm-plus/gplus"
 	"github.com/kataras/iris/v12"
 	"github.com/patrickmn/go-cache"
 )
@@ -31,11 +32,22 @@ func (ic *IVodController) getVideos(ctx iris.Context) {
 	web.NewData(data).Build(ctx)
 }
 
+func (ic *IVodController) getDetail(ctx iris.Context) {
+	id, _ := handler.NewIDWithContext(ctx)
+	result, gb := gplus.SelectById[repos.VideoRepo](id)
+	if gb.Error != nil {
+		web.NewError(gb.Error).Build(ctx)
+		return
+	}
+	web.NewData(result).Build(ctx)
+}
+
 func Register(u iris.Party) {
 	vod := newVod()
 
 	// Deprecated: remove this
 	u.Get("/home", vod.renderHome).Name = "代理访问首页"
 
-	u.Get("/videos", vod.getVideos)
+	u.Get("/videos", vod.getVideos).Name = "获取视频"
+	u.Get("/video/{id:uint}", vod.getDetail).Name = "获取视频详情"
 }
