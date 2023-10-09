46f45f4b86f54961ed7326ba45aac2b448564667
d1y <chenhonzhou@gmail.com>
2023-10-09 21:45:50 +0800

    web: add utils unit(with Pagination)

--- controllers/handler/utils_test.go
+++ controllers/handler/utils_test.go
@@ -1,1 +1,16 @@
-package handler_test
+package handler
+
+import (
+	"testing"
+)
+
+func TestNewPagination(t *testing.T) {
+	pg1 := newPagination()
+	if pg1.Page != 1 || pg1.Limit != 20 {
+		t.FailNow()
+	}
+	pg2 := newPagination(2, 42)
+	if pg2.Page != 2 || pg2.Limit != 42 {
+		t.FailNow()
+	}
+}
